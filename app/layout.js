d06faf751f328754f1b3d07483c98182af4b008a
Tushar Jain <112325029+tushar19jain@users.noreply.github.com>
2024-01-31 18:52:56 +0530

    vsFbvFS

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <script src="https://cdn.tailwindcss.com"></script>
     </html>
   );
 }
