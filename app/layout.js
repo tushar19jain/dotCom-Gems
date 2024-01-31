3d39705d451188653f8e2a34b63556ad31b1ae93
Tushar Jain <112325029+tushar19jain@users.noreply.github.com>
2024-01-31 18:57:22 +0530

    dsVWDfsW

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <script src="https://cdn.tailwindcss.com"></script>
     </html>
   );
 }
